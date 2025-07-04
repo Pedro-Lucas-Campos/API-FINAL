d6c435518a4d4755c55716b8a9b3502cffb5b40c
Pedro-Lucas-Campos <peashtazer@gmail.com>
2025-07-04 09:03:00 -0300

    ajuste utc

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import datetime
+from datetime import timezone 
 from zoneinfo import ZoneInfo
 
 app = FastAPI(
     title="API de Registro de Veículos",
     description="Recebe dados de VIN e Localização e os salva em um arquivo CSV central."
 )
+
 database = []
 
 class Record(BaseModel):
@@ -16,11 +18,18 @@ class Record(BaseModel):
 
 @app.post("/add_record", status_code=201)
 def registrar_dado(dado: Record):
-    fuso_horario = ZoneInfo("America/Sao_Paulo")
+    timestamp_utc = datetime.datetime.now(timezone.utc)
+
+    fuso_horario_sp = ZoneInfo("America/Sao_Paulo")
+    timestamp_sp = timestamp_utc.astimezone(fuso_horario_sp)
+
     dado_dict = dado.model_dump()
-    timestamplocal = datetime.datetime.now(fuso_horario)
-    dado_dict["timestamp"] = timestamplocal.isoformat()
+
+    dado_dict["timestamp_utc"] = timestamp_utc.isoformat()
+    dado_dict["timestamp_sao_paulo"] = timestamp_sp.isoformat()
+    
     database.append(dado_dict)
+    
     return {"mensagem": "Dado recebido com sucesso", "dados": dado_dict}
 
 @app.get("/dados")
